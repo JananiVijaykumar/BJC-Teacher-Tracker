bd8931ceaabd4f1e16edea39639f45ba3b7196a2
= <zach.chao@yahoo.com>
2019-11-26 14:20:17 -0800

    progress, view and js seems to be done, now just need to pull coords from model, may need to create and cache or find on user creation

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,17 @@ class MainController < ApplicationController
     def index
         @teacher = Teacher.new
     end
+
+    def dashboard
+        @items = [
+            {
+                'long': 39,
+                'lat': -95,
+            },
+            {
+                'long': 37,
+                'lat': -92,
+            }
+        ]
+    end
 end
